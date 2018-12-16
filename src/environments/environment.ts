faf26395ebb7e6e03f1ef5e3dc0cb1b78ec1521d
Zachary Graham <zakgraham@gmail.com>
2018-12-16 12:53:00 -0700

    comment out production values

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-	production: false,
+	// production: false,
 
 	dialogflow: {
 		angularBot: 'fb9afd29c4bf4670bc19bf0738a40bd8'
